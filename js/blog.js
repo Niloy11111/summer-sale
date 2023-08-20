9639e19f4c3eafb82f53f4dd37402f1859a2ad6a
Niloy11111 <Ektiaruddinniloy859@gmail.com>
2023-08-21 04:51:58 +0600

    added new js code

--- js/blog.js
+++ js/blog.js
@@ -7,29 +7,13 @@ function setAppendText(target){
 
     const productName = document.getElementById('append-div');
 
-    const li = document.createElement('li');
-    li.innerText = productTitle ;
-    li.classList.add('mt-2','text-xl','text-black-primary','font-semibold','font-work');
-    productName.appendChild(li);  
+    const ol = document.createElement('ol');
+    ol.innerText = productTitle ;
+    ol.classList.add('mt-2','text-xl','text-black-primary','font-semibold','font-work');
+    productName.appendChild(ol);  
     
     setTotalPrice(productPriceValue);
-    
-    
-
-//     const li = document.createElement('li');
-//     li.innerText = target.childNodes[3].childNodes[3].innerText;
-//     li.classList.add('mt-2','text-xl','text-black-primary','font-semibold','font-work');
-
-//     const productNum = document.getElementById('append-div');
-//     productNum.appendChild(li);
-    
-//    const totalPrice = document.getElementById('total-Price');
-//    const totalPriceString = target.childNodes[3].childNodes[5].innerText.split(' ')[0] ;
-//    const totalPriceNumber = parseFloat(totalPriceString); 
-
-
 }
-
 
 function setTotalPrice(productPrice){
     const totalPriceElement = document.getElementById('total-Price');
@@ -41,38 +25,40 @@ function setTotalPrice(productPrice){
 
     totalPriceElement.innerText = currentTotalAmount.toFixed(2) ;
 
+    const afterDiscountElement = document.getElementById('totalAfterDiscount') ;
+    afterDiscountElement.innerText = currentTotalAmount.toFixed(2) ;
+
     const deleteButton = document.getElementById('btn-purchase');
+
     if(currentTotalAmount > 0){
         deleteButton.removeAttribute('disabled');
     }
-    if(currentTotalAmount >= 200){
-        applyButton.removeAttribute('disabled');
-    }
-    const applyButton = document.getElementById('btn-apply').addEventListener('click', function(){
-     
 
+    const applyButton = document.getElementById('btn-apply');
+     if(currentTotalAmount >= 200){
+            applyButton.removeAttribute('disabled');
+        }
+
+     document.getElementById('btn-apply').addEventListener('click', function(){
         const couponField = document.getElementById('coupon-field');
         const couponFieldValue = couponField.value ;
 
         if(couponFieldValue === 'SELL200'){
         const discountElement = document.getElementById('discount-element');
         const totalDiscount = currentTotalAmount * 0.20 ;
-        discountElement.innerText = totalDiscount ;
+        discountElement.innerText = totalDiscount.toFixed(2);
+
+        const afterDiscountElement = document.getElementById('totalAfterDiscount') ;
+        const currentAfterDiscount = currentTotalAmount - totalDiscount ; 
+
+        afterDiscountElement.innerText = currentAfterDiscount.toFixed(2) ; 
         }
-
     })
-    
-
-    
-        
-        
-        // if(couponFieldValue === 'SELL200'){
-        //     const discountElementString = discountElement.innerText ;
-        //     const discountElementValue = parseFloat(discountElementString);
-        // }
-    
-    
 }
+
+
+
+
 
  
 
